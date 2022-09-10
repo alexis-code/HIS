79047321a2071e065b13b99eb12885429234dcef
alexis-code <alexir325@gmail.com>
2022-09-10 17:11:46 -0400

    change to prod

--- his/his/settings/base.py
+++ his/his/settings/base.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-from .local import *
+from .production import *
 
 # Application definition
 
